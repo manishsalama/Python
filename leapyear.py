ddba74f730d8fdb0a1b9859d3d7e389939da6668
Manish Reddy Salama <manishreddysalama@gmail.com>
2026-04-01 19:11:15 +0530

    Update leapyear.py
    Resolved merge conflict in leapyear.py by consolidating duplicate code sections and ensuring proper indentation.

--- leapyear.py
+++ leapyear.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
+
 year = int(input("Enter a year: "))
 
-# Check the leap year conditions
 if (year % 4 == 0):
     if (year % 100 == 0):
         if (year % 400 == 0):
@@ -14,7 +13,6 @@ else:
 =======
 year = int(input("Enter a year: "))
 
-# Check the leap year conditions
 if (year % 4 == 0):
     if (year % 100 == 0):
         if (year % 400 == 0):
@@ -24,5 +22,4 @@ if (year % 4 == 0):
     else:
         print(year, "is a leap year")
 else:
->>>>>>> 813ba899763f67ca7c4c4aa841c8ea7ba1636e9c
     print(year, "is not a leap year")
