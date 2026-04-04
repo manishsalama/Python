435a83ef86aca2d568bba7a1e43d1209af2170d0
Manish Reddy Salama <manishreddysalama@gmail.com>
2026-04-04 20:14:24 +0530

    Add script to square and sort a list of numbers

---
+++ Assignment-3 Python/square no.py
@@ -0,0 +1,7 @@
+numbers = [-4, -1, 0, 3, 10]
+
+squared_and_sorted = sorted([num ** 2 for num in numbers])
+
+print(f"Original numbers: {numbers}")
+print(f"Squared and sorted: {squared_and_sorted}")
+print("\n")
